a3976aaaaca78ef86f30b8412155982a702690c5
mdbow22 <mdbow22@gmail.com>
2021-11-01 08:24:26 -0400

    created login route

--- controllers/api_routes/user_routes.js
+++ controllers/api_routes/user_routes.js
@@ -25,4 +25,41 @@ router.post('/', async (req, res) => {
     }
 });
 
+//login an existing user
+router.post('/login', async (req, res) => {
+    try {
+        //search for user in db
+        const theUser = await User.findOne({
+            where: {
+                username: req.body.username
+            }
+        });
+
+        //send back 404 if user not found
+        if(theUser === null) {
+            res.status(404).json('Username or password is incorrect');
+            return;
+        }
+
+        //verify that password passed matches username's pw
+        const matches = await bcrypt.compare(req.body.password, theUser.password);
+
+        //send back 404 if password is a mismatch
+        if(!matches) {
+            res.status(404).json('Username or password is incorrect');
+            return;
+        }
+
+        //save the session so they're logged in
+        req.session.save(() => {
+            req.session.loggedIn = true;
+            res.status(200).json('Now logged in');
+        })
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json('Unable to log in');
+    }
+});
+
 module.exports = router;

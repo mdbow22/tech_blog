161d3f96e22c1d2f3e50d2f5f981f7a40ef41a57
mdbow22 <mdbow22@gmail.com>
2021-11-01 21:37:10 -0400

    added dashboard view and route

--- controllers/home_routes.js
+++ controllers/home_routes.js
@@ -1,3 +1,5 @@
+const { Post } = require('../models');
+
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
@@ -12,6 +14,42 @@ router.get('/', (req, res) => {
 
 router.get('/login', (req, res) => {
     res.render('login');
-})
+});
+
+router.get('/dashboard', async (req, res) => {
+
+    try {
+        //make sure user is logged in
+        if(!req.session.loggedIn) {
+            res.redirect('/login');
+            return;
+        }
+
+        //retrieve all of the users posts from db
+        const userPosts = await Post.findAll({
+            where: {
+                author_id: 1 //req.session.userId
+            }
+        });
+
+        
+        //clean up data and send to client
+        if(userPosts.length > 0) {
+            const posts = userPosts.get({ plain: true });
+            res.render('dashboard', {posts, loggedIn: req.session.loggedIn});
+        } else {
+            res.render('dashboard', {
+                loggedIn: req.session.loggedIn,
+                noPosts: true    
+            });
+        }
+
+        
+        
+
+    } catch (err) {
+        res.status(500).json('error loading page');
+    }
+});
 
 module.exports = router;

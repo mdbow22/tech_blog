a29bdaa0692a3234c3589c53695452897af124d7
mdbow22 <mdbow22@gmail.com>
2021-11-03 19:57:21 -0400

    added comments

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,10 +1,12 @@
 module.exports = {
     format_date: (date) => {
+        //parse incoming date into month day year
         let formatDate = new Date(date);
         let year = formatDate.getFullYear();
         let month = formatDate.getMonth() + 1;
         let day = formatDate.getDate();
-    
+        
+        //return just those values, so time is not displayed
         return `${month}/${day}/${year}`;
     }
 };
